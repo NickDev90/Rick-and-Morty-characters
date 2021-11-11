40ac3db29af0d538ecc44a220ef9f9305847b7bd
NickDev90 <nickie05021990@gmail.com>
2021-11-11 18:57:21 +0100

    onClick on character

--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -2,19 +2,14 @@ import React from 'react';
 import styles from './Character.module.css'; 
 import {useHistory} from 'react-router-dom';
 
-
-
-
 const Character = ({hero}) => {
 
    const history = useHistory();
 
 	return (
-  				<div className={styles.heroInfo}>
+  				<div className={styles.heroInfo} onClick={() => history.push(`hero/${hero.id}`)} >
                      <span>
-                        <img src={hero.image} className={styles.heroPhoto} 
-                           onClick={() => history.push(`hero/${hero.id}`)}
-                        />
+                        <img src={hero.image} className={styles.heroPhoto} />
                      </span>                 
                      <span className={styles.heroName}>{hero.name}</span>
                      <span>{hero.gender}</span>
@@ -23,6 +18,5 @@ const Character = ({hero}) => {
   				</div>
    )
 }
-
 
 export default Character;
